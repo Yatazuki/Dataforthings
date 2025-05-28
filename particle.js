c8ce08365be6e82a078d21afd432d1eaad04c415
Yatazuki <yatazuki.yamata@gmail.com>
2025-05-28 08:17:17 +0200

    Increase particle visibility

--- particle.js
+++ particle.js
@@ -21,22 +21,22 @@ particlesJS("particles-js", {
         }
       },
       "opacity": {
-        "value": 0.35,
+        "value": 0.6,
         "random": true,
         "anim": {
           "enable": true,
           "speed": 0.8,
-          "opacity_min": 0.1,
+          "opacity_min": 0.2,
           "sync": false
         }
       },
       "size": {
-        "value": 4,
+        "value": 5,
         "random": true,
         "anim": {
           "enable": true,
           "speed": 2,
-          "size_min": 0.5,
+          "size_min": 0.8,
           "sync": false
         }
       },
@@ -44,8 +44,8 @@ particlesJS("particles-js", {
         "enable": true,
         "distance": 170,
         "color": "#8000ff",
-        "opacity": 0.3,
-        "width": 1
+        "opacity": 0.4,
+        "width": 1.2
       },
       "move": {
         "enable": true,
